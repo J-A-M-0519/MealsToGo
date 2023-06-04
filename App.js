2db85458e86e2ec5251b762b7a67875681d5e27b
J-A-M-0519 <Alex.miranda7965@gmail.com>
2023-06-04 10:50:42 -0400

    new file:   .eslintrc 	modified:   App.js 	new file:   jsconfig.json 	modified:   package-lock.json 	modified:   package.json 	deleted:    src/.eslintrc 	new file:   src/features/restaurants/components/restaurant-info-card.component.js 	new file:   src/features/restaurants/screens/restaurants.screen.js 	new file:   yarn-error.log 	modified:   yarn.lock

--- App.js
+++ App.js
@@ -1,27 +1,13 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React from "react";
+
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-        <View style={{ padding: 16, backgroundColor: "yellow" }}>
-          <Text>Search</Text>
-        </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
